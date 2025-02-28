77a57fdd4be52c45d9cf98034c73f5cc43c93875
joaootaviofm <joaootaviofm@gmail.com>
2025-02-28 13:51:05 -0300

    fixing dropdown skills

--- src/_components/Navbar.jsx
+++ src/_components/Navbar.jsx
@@ -1,56 +1,60 @@
-"use client"
+"use client";
 
 import { FaReact } from "react-icons/fa";
 import { RiNextjsFill } from "react-icons/ri";
 import { RiTailwindCssFill } from "react-icons/ri";
-import { motion } from "motion/react"
-import { useState } from 'react'
+import { FaArrowDown } from "react-icons/fa6";
+import { FaArrowUp } from "react-icons/fa";
+
+import { motion } from "motion/react";
+import { useState } from "react";
 
 export default function Navbar() {
+  const [skillsOpen, setSkillsOpen] = useState(false);
 
+  const navList = [
+    { item: "About", path: "#about" },
+    { item: "Home", path: "#home" },
+    { item: "Skills", path: "#skills" },
+  ];
 
-    const [skillsOpen, setSkillsOpen] = useState(false)
-
-    function toggleSkills() {
-        setSkillsOpen(!skillsOpen)
-    }
-
-
-    const navList = [
-        { item: "About", path: "#about" },
-        { item: "Home", path: "#home" },
-        { item: "Skills", path: "#skills" }
-    ]
-
-    return (
-        <nav className="relative flex justify-center py-10 bg-[#2B2A27] text-white">
-            <ul className="flex gap-11">
-                {navList.map((item, index) => (
-                    <li key={index}>
-                        <a
-                            className="font-bold"
-                            href={item.path == "#skills" ? "#" : item.path}
-                            onClick={item.item == "Skills" ? toggleSkills : null}
-                        >
-                            {item.item}
-                        </a>
-                    </li>
-                ))}
-            </ul>
-            {skillsOpen && (
-                <div className="absolute top-30">
-                    <div className='flex flex-col rounded-lg bg-white text-black pb-5'>
-                        <h1 className='text-2xl font-semibold p-7'>
-                            Technologies used in this project
-                        </h1>
-                        <motion.div className="flex items-center justify-center gap-1 text-3xl">
-                            <FaReact />
-                            <RiNextjsFill />
-                            <RiTailwindCssFill />
-                        </motion.div>
-                    </div>
-                </div>
-            )}
-        </nav>
-    )
+  return (
+    <nav className="relative flex justify-center py-10 bg-[#2B2A27] text-white">
+      <ul className="flex gap-11">
+        {navList.map((item, index) => (
+          <li key={index}>
+            <a
+              className="font-bold flex items-center justify-center"
+              href={item.path == "#skills" ? "#" : item.path}
+              onMouseEnter={() => item.item == "Skills" && setSkillsOpen(true)}
+              onMouseLeave={() => item.item == "Skills" && setSkillsOpen(false)}
+            >
+              {item.item}
+              {item.item == "Skills" && <span className="text-sm"></span>}
+            </a>
+          </li>
+        ))}
+      </ul>
+      {skillsOpen && (
+        <motion.div
+          initial={{ y: -30, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.4 }}
+          exit={{ y: -30, opacity: 0 }}
+          className="absolute top-30"
+        >
+          <div className="flex flex-col rounded-lg bg-white text-black pb-5 drop-shadow-lg">
+            <h1 className="text-2xl font-semibold p-7">
+              Technologies used in this project
+            </h1>
+            <div className="flex items-center justify-center gap-1 text-3xl">
+              <FaReact />
+              <RiNextjsFill />
+              <RiTailwindCssFill />
+            </div>
+          </div>
+        </motion.div>
+      )}
+    </nav>
+  );
 }
